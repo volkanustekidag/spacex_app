2c6a8e6e02f2ac918b6fbd9add776b8fd3796ddb
volkanustekidag <volkanustekidg@gmail.com>
2022-04-12 23:47:05 +0300

    spacex repository has been created

---
+++ lib/repositories/spacex_repositories.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:spacex_app/models/latest.dart';
+
+const url = 'https://api.spacexdata.com/v4/launches/latest';
+
+class SpacexRepositories {
+  final http.Client httpClient;
+  SpacexRepositories(this.httpClient);
+
+  Future<Latest> fetchLatest() async {
+    final response = await this.httpClient.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final latestJson = jsonDecode(response.body);
+      return Latest.fromJson(latestJson);
+    } else {
+      throw Exception('Error!');
+    }
+  }
+}
